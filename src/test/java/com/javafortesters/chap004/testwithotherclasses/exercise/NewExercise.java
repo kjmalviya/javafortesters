8c75d42c3206a9700b8b1434f6b5fd877fa41d11
Kunal <kunal.sscet@gmail.com>
2016-05-27 12:53:04 -0700

    Rejected commit: Default

--- src/test/java/com/javafortesters/chap004/testwithotherclasses/exercise/NewExercise.java
+++ src/test/java/com/javafortesters/chap004/testwithotherclasses/exercise/NewExercise.java
@@ -13,5 +13,14 @@ public class NewExercise {
     public void hexString(){
         Integer eleven = 11;
         assertEquals("11 becomes b","b", eleven.toHexString(11));
+
+        Integer ten = new Integer(10);
+        assertEquals("10 becomes a", "a", ten.toHexString(10));
+
+        Integer three = 3;
+        assertEquals("3 becomes 3", "3", three.toHexString(3));
+
+        Integer twentyone = 21;
+        assertEquals("21 becomes 15", "15", twentyone.toHexString(21));
    }
 }
